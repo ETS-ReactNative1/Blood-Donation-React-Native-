3d4434db0c6ce6aa5bd98eba170bf42f256fde14
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-10-29 00:47:39 +0600

    onboarding screen

--- src/Screens/OnBoarding/index.js
+++ src/Screens/OnBoarding/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, View, StatusBar, StyleSheet} from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
 
 const OnBoarding = () => {
+  const onboardingImages = [
+    {
+      backgroundColor: '#FFF',
+      image: (
+        <Image source={require('../../../assets/image/onborading1.png')} />
+      ),
+      title: 'Find Blood Doner',
+      subtitle: 'Done with React Native Onboarding Swiper'
+    },
+    {
+      backgroundColor: '#FFF',
+      image: <Image source={ require('../../../assets/image/onboarding2.png') } />,
+      title: 'Post For Blood Request',
+      subtitle: 'Done with React Native Onboarding Swiper'
+    }
+  ];
   return (
-    <View>
-      <Text>Hello Learning</Text>
-      <Text>Hello Learning</Text>
-      <Text>Hello Learning</Text>
-      <Text>Hello Learning</Text>
-    </View>
+   <View style={styles.main}>
+     <StatusBar backgroundColor="white" barStyle="dark-content" />
+     <Onboarding 
+      pages={onboardingImages}
+      onDone={ ()=>console.warn("done") }
+      onSkip={ ()=>console.warn("Skip") }
+     />
+   </View>
   );
+  
+  
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  main:{
+    height: '100%',
+    width: '100%'
+  }
+});
 
 export default OnBoarding;
