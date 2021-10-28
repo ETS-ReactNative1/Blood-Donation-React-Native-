48752f387a78f4ca4c4e536b41b556789f983007
Md. Rabiul Hasan <rabiul.fci@gmail.com>
2021-10-28 23:53:38 +0600

    Hello World Print

---
+++ src/Screens/OnBoarding/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const OnBoarding = () => {
+  return (
+    <View>
+      <Text>Hello Learning</Text>
+      <Text>Hello Learning</Text>
+      <Text>Hello Learning</Text>
+      <Text>Hello Learning</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default OnBoarding;
